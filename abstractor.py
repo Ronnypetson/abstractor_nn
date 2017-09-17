241281fdd37fc7fd924baa54f6a6ffcc61c15639
Ronnypetson <rsronnypetson4@gmail.com>
2017-09-16 21:29:44 -0300

    update

--- abstractor.py
+++ abstractor.py
@@ -1,3 +1,6 @@
+import numpy as np
+import tensorflow as tf
+
 class AbGraph:
     def __init__(self,input_len,output_len):
         self.id_count = input_len+output_len
@@ -6,6 +9,11 @@ class AbGraph:
         self.nodes = set() # Nodes identification
         self.front = set()  # Nodes that can be removed
         self.abstractors = {}   # Only one abstractor per pair of nodes
+        self.X = tf.placeholder(tf.float32,shape=(None,input_len))
+        self.Y = tf.placeholder(t.float32,shape=(None,output_len))
+        self.weights = {}    # Weights and biases of each abstractor
+        self.biases = {}
+        self.activations = {}   # the output of the nodes
         for i in range(self.id_count):
             self.nodes.add(i)   # 0 to input_len-1, input_len to input_len+output_len-1
             self.front.add(i)
@@ -31,4 +39,25 @@ class AbGraph:
             self.nodes.remove(y)
             return True
         return False
+
+    def build_model(self):
+        # Setup parameters
+        for k in self.abstractors.keys():   # Weights of abstractor k
+            self.weights[k] = tf.Variable(tf.random_normal( (len(k),1) )) # mean = 0.0, sd = 1.0
+            self.biases[k] = tf.Variable(0.0)
+        # Define activations
+        for i in range(self.input_len):
+            self.activations[i] = self.X[:,i]   #
+        for k in self.abstractors.keys():
+            parents = tf.Variable([0.0]*len(k))    #
+            for i in range(len(k)):
+                parents[i] = self.activations[i]
+            child = self.abstractions[k]
+            self.activations[child] = tf.nn.elu(tf.matmul(parents,self.weights[k])+)
+
+#g = AbGraph(5,3)
+#print(g.insert_ab((0,1)))
+#for i in g.abstractors.keys():
+#    print(i,g.abstractors[i])
+#print(g.delete_ab((0,1)))
 
