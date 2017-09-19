ad669a0e9be0dffc7469229a98c2a92fc767e0d8
Ronnypetson <rsronnypetson4@gmail.com>
2017-09-18 23:32:21 -0300

    update

--- abstractor.py
+++ abstractor.py
@@ -1,7 +1,9 @@
 import numpy as np
 import tensorflow as tf
+import random as rd
 
 batch_size = 10 #
+learning_rate = 0.01
 class AbGraph:
     def __init__(self,input_len,output_len):
         self.id_count = input_len   #+output_len
@@ -55,28 +57,63 @@ class AbGraph:
             parents = []   #
             for i in range(len(k)):
                 parents.append( self.activations[k[i]] )
-            parents = tf.Variable(tf.transpose( parents ))
-            if len(parents.shape) == 3: # Gambiarra
-                parents = parents[0]
+            self.parents = tf.Variable(tf.transpose( parents ))
+            if len(self.parents.shape) == 3: # Gambiarra
+                self.parents = self.parents[0]
             #print(parents)
             child = self.abstractors[k]
-            self.activations[child] = tf.nn.elu(tf.matmul(parents,self.weights[k])+self.biases[k])
+            self.activations[child] = tf.nn.elu(tf.matmul(self.parents,self.weights[k])+self.biases[k])
         # Fully connected at the end (front -> output)
         front_ = [] # tf.Variable(np.zeros( (len(self.front)) ))
         for f in self.front:
             front_.append(self.activations[f])
-        front_ = tf.Variable(tf.transpose( front_ ))
-        if len(front_.shape) == 3:  # Gambiarra
-                front_ = front_[0]
-        self.Y = tf.nn.elu(tf.matmul(front_,self.weights[(-1,)])+self.biases[(-1,)])
+        self.front_ = tf.Variable(tf.transpose( front_ ))
+        if len(self.front_.shape) == 3:  # Gambiarra
+                self.front_ = self.front_[0]
+        self.output = tf.nn.elu(tf.matmul(self.front_,self.weights[(-1,)])+self.biases[(-1,)])
 
-g = AbGraph(3,2)
+    def var_init(self):
+        var = []
+        for w in self.weights:
+            var.append(self.weights[w])
+            #tf.variables_initializer([self.weights[w]])
+        for b in self.biases:
+            var.append(self.biases[b])
+            #tf.variables_initializer([self.biases[b]])
+        #var.append(self.parents)
+        var.append(self.front_)
+        return tf.variables_initializer(var)
+
+g = AbGraph(3,1)
 print(g.insert_ab((0,1)))
 print(g.insert_ab((1,2)))
 print(g.insert_ab((3,4)))
 for i in g.abstractors.keys():
     print(i,g.abstractors[i])
 g.build_model()
+
+def get_batch():
+    X = np.zeros((batch_size,3))
+    Y = np.zeros((batch_size,1))
+    for i in range(batch_size):
+        x = rd.uniform(0.0,1.0)
+        y = rd.uniform(0.0,1.0)
+        z = rd.uniform(0.0,1.0)
+        s = x + y + z
+        X[i] = [x,y,z]
+        Y[i] = s
+    return X,Y
+
+cost = tf.losses.mean_squared_error(g.Y,g.output)
+opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(g.var_init())
+    for i in range(1000):
+        X,Y = get_batch()
+        loss = sess.run(opt,feed_dict={g.X:X,g.Y:Y})
+        if i%50 == 0:
+            print(loss)
 
 #print(g.delete_ab((0,1)))
 
