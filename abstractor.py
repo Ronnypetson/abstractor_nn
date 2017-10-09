142dcc76412e719465e264abdbfe79e6fa53f80d
Ronnypetson <rsronnypetson4@gmail.com>
2017-10-08 23:39:00 -0300

    fixed session graph loading

--- abstractor.py
+++ abstractor.py
@@ -71,24 +71,25 @@ class AbGraph:
             return True
         return False
     
-    def reset_model(self):
-        self.weights = {}    # Weights and biases of each abstractor
-        self.biases = {}
-        self.params_named = {}
-        self.activations = {}   # the output of the nodes
+    def ab_name(self,k):
+        s = '.'
+        for i in k:
+            s = s + str(i) + '.'
+        return s
     
     def build_model(self):
-        self.reset_model()
         for k in self.abstractors.keys():   # Setup parameters # Weights of abstractor k
-            self.weights[k] = tf.Variable(tf.random_normal( (len(k),1) )) # mean = 0.0, sd = 1.0
-            self.biases[k] = tf.Variable(np.zeros((1,1),dtype=np.float32))   #
-            self.params_named['w_'+str(k)] = self.weights[k]
-            self.params_named['b_'+str(k)] = self.biases[k]
+            k_name = self.ab_name(k)
+            self.weights[k] = tf.Variable(tf.random_normal( (len(k),1) ),name='w_'+ k_name) # mean = 0.0, sd = 1.0
+            self.biases[k] = tf.Variable(np.zeros((1,1),dtype=np.float32),name='b_'+ k_name)   #
+            self.params_named['w_'+k_name] = self.weights[k]
+            self.params_named['b_'+k_name] = self.biases[k]
         # Parameters of the final layer (fully-connecetd)
-        self.weights[(-1,)] = tf.Variable(tf.random_normal( (len(self.front),self.output_len) ))
-        self.biases[(-1,)] = tf.Variable(np.zeros((1,self.output_len),dtype=np.float32))   #
-        self.params_named['w_'+str((-1,))] = self.weights[(-1,)]
-        self.params_named['b_'+str((-1,))] = self.biases[(-1,)]
+        fc_name = self.ab_name((-1,))
+        self.weights[(-1,)] = tf.Variable(tf.random_normal( (len(self.front),self.output_len) ),name='w_'+fc_name)
+        self.biases[(-1,)] = tf.Variable(np.zeros((1,self.output_len),dtype=np.float32),name='b_'+fc_name)   #
+        self.params_named['w_'+fc_name] = self.weights[(-1,)]
+        self.params_named['b_'+fc_name] = self.biases[(-1,)]
         for i in range(self.input_len):     # Define activations
             l = []
             for j in range(batch_size):
@@ -115,45 +116,37 @@ class AbGraph:
         for k in self.biases:
             sess.run(self.biases[k].initializer)
 
-g = AbGraph.from_size(20,1)
-#g.insert_ab((0,1))
-#g.insert_ab((1,2))
-#g.insert_ab((0,2))
-#print(g.nodes)
-#print(g.front)
-#for i in range(19): # 0 - 19
-#    g.insert_ab((i,i+1))
-#for i in range(20,38):
-#    g.insert_ab((i,i+1))
-#for i in range(39,56):
-#    g.insert_ab((i,i+1))
-g.build_model()
-
 def get_batch():
-    X = np.zeros((batch_size,20))
+    X = np.zeros((batch_size,1))
     Y = np.zeros((batch_size,1))
     for i in range(batch_size):
         s = 0.0
-        for j in range(20):
-            X[i,j] = rd.uniform(0.0,10.0)
+        for j in range(1):
+            X[i,j] = rd.uniform(0.0,1.0)
             s += X[i,j]
+        #X[i,0] = rd.uniform(0.0,1.0)
+        #s = X[i,0]*(1-X[i,1])+(1-X[i,0])*X[i,1]
+        #s = X[i,0]+X[i,1]
+        #s = X[i,0]
+        #s = rd.uniform(0.0,1.0)
         Y[i] = [s]
     return X,Y
 
 def get_cost_opt(g_):
-    cost = tf.losses.mean_squared_error(g_.Y,g_.output)
+    cost = tf.losses.mean_squared_error(g_.Y,g_.output) #losses.mean_squared_error(g_.Y,g_.output)
     opt = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
     return cost, opt
 
-cost_g, opt_g = get_cost_opt(g)
-saver = tf.train.Saver(g.params_named)  # saver is associated with g.params_named variables
-
-with tf.Session() as sess:
-    if os.path.isfile(model_g_fn+'.meta'):
-        saver.restore(sess,model_g_fn)
-    else:
-        sess.run(tf.global_variables_initializer())
-    for i in range(6000):
+with tf.Session() as sess:  # sess graph != AbGraph
+    g = AbGraph.from_size(1,1)
+    g.build_model()
+    cost_g, opt_g = get_cost_opt(g)
+    saver = tf.train.Saver() # g.params_named;  #saver is associated with g.params_named variables
+    #if os.path.isfile(model_g_fn+'.meta'):
+    #    saver.restore(sess,model_g_fn)
+    #else:
+    sess.run(tf.global_variables_initializer())
+    for i in range(11000):
         X,Y = get_batch()
         loss,_ = sess.run([cost_g,opt_g],feed_dict={g.X:X,g.Y:Y})
         if i%50 == 0:
@@ -162,26 +155,30 @@ with tf.Session() as sess:
                 saver.save(sess,model_g_fn)
     print()
 
+tf.reset_default_graph()
 with tf.Session() as sess:
-    h = AbGraph.from_graph(g)
     #
+    h = AbGraph.from_graph(g)   # same structure as g, loads the operations declared on g
+    #h.init_var(sess)
+    saver_h = tf.train.Saver()  # g.params_named
+    saver_h.restore(sess,model_g_fn)    # restore from g
     #
+    #h.insert_ab((0,))
+    #h.insert_ab((1,))
+    #h.insert_ab((0,2))
+    #h.insert_ab((1,3))
     #del h.params_named['w_(-1,)']  # restore a subset of the parameters from g
     #del h.params_named['b_(-1,)']
     #
-    h.build_model()
     cost_h, opt_h = get_cost_opt(h)
-    h.init_var(sess)    # modifica o grafo, atualiza o modelo, inicializa variaveis, restaura variaveis
+    # modifica o grafo, atualiza o modelo, inicializa variaveis, restaura variaveis
     #
-    saver_h = tf.train.Saver(g.params_named)
-    saver_h.restore(sess,model_g_fn)    # restore from g
-    #
-    for i in range(1500):
+    for i in range(6000):
         X,Y = get_batch()
         loss,_ = sess.run([cost_h,opt_h],feed_dict={h.X:X,h.Y:Y})
         if i%50 == 0:
             print(loss)
-            if i%1000 == 0:
+            if i%5000 == 0:
                 saver_h.save(sess,model_h_fn)
     print()
 
